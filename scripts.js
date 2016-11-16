a7c022e8aa9ca4eb4d059d36e502244d26b10ea2
Colin Arpe <colin.j.arpe@outlook.com>
2016-11-16 15:54:03 -0500

    Add timer to reveal popup

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ var popup = document.getElementById("modal-window");
 
 trigger.addEventListener("click", revealWindow);
 
+setTimeout (revealWindow, 2000);
+
 function revealWindow (event)	{
 	popup.style.visibility = "visible";
 }
