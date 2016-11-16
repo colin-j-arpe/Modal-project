99a01b6d4c83676a70464ca3d6a667fd8c123709
Colin Arpe <colin.j.arpe@outlook.com>
2016-11-16 16:40:44 -0500

    Add close button and functionality to popup window

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
 var trigger = document.getElementById("head-logo")
 var popup = document.getElementById("modal-window");
+var close = document.getElementById("close-button");
 
 trigger.addEventListener("click", revealWindow);
 
 setTimeout (revealWindow, 2000);
 
+close.addEventListener("click", removeWindow);
+
 function revealWindow (event)	{
 	popup.style.visibility = "visible";
+}
+
+function removeWindow (event)	{
+	popup.style.visibility = "hidden";
 }
