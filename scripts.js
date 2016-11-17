b07d97b5aaf9669f43960901be5ea058cf1c903c
Colin Arpe <colin.j.arpe@outlook.com>
2016-11-17 12:16:18 -0500

    Closes #2

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ var newAccount = document.getElementById("account-button");
 var close = document.getElementById("close-button");
 var account = document.getElementById("account-window");
 var passwordArea = document.getElementsByName("password");
-var	check = document.getElementById("checkOrX");
+	var	check = document.getElementById("checkOrX");
 var cancel = document.getElementById("cancel-button");
 
 trigger.addEventListener("click", revealLogIn);
@@ -38,19 +38,28 @@ function removeAccount (event)	{
 
 function comparePasswords (event)	{
 	check.style.visibility = "visible";
-	if (document.getElementsByName("password")[1].value.length === document.getElementsByName("password")[2].value.length)	{
-		if (samePassword())
+	if (sameLength())	{
+		// console.log("checking pwd");
+		if (samePassword())	{
 			check.innerHTML = "<img src='Images/check.png' width=15px height=15px>";
+		}
 	}	else 	{
 		check.innerHTML = "<img src='Images/x.png' width=15px height=15px>";
 	}
 }
 
+function sameLength ()	{
+	if (document.getElementsByName("password")[1].value.length === document.getElementsByName("password")[2].value.length)
+		return true;
+	else
+		return false;
+}
+
 function samePassword ()	{
 	for (var i = 0; i < document.getElementsByName("password")[1].value.length; i++) {
+		// console.log(document.getElementsByName("password")[1].value[i] + " " + document.getElementsByName("password")[2].value[i])
 		if (document.getElementsByName("password")[1].value[i] !== document.getElementsByName("password")[2].value[i])
 			return false;
-		else
-			return true;
 	}
+	return true;
 }
