8bf02a6fdb8d0132ccb96bdcfffa6c165700f698
Colin Arpe <colin.j.arpe@outlook.com>
2016-11-17 01:48:44 -0500

    Add dynamic new password confirm functionality

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ var logIn = document.getElementById("login-window");
 var newAccount = document.getElementById("account-button");
 var close = document.getElementById("close-button");
 var account = document.getElementById("account-window");
+var passwordArea = document.getElementsByName("password");
+var	check = document.getElementById("checkOrX");
 var cancel = document.getElementById("cancel-button");
 
 trigger.addEventListener("click", revealLogIn);
@@ -12,6 +14,8 @@ setTimeout (revealLogIn, 500);
 newAccount.addEventListener("click", revealAccount);
 close.addEventListener("click", removeLogIn);
 cancel.addEventListener("click", removeAccount);
+
+passwordArea[2].addEventListener("keyup", comparePasswords);
 
 function revealLogIn (event)	{
 	logIn.style.visibility = "visible";
@@ -29,4 +33,23 @@ function revealAccount (event)	{
 function removeAccount (event)	{
 	account.style.visibility = "hidden";
 	revealLogIn();
+}
+
+function comparePasswords (event)	{
+	check.style.visibility = "visible";
+	if (document.getElementsByName("password")[1].value.length === document.getElementsByName("password")[2].value.length)	{
+		if (samePassword())
+			check.innerHTML = "<img src='Images/check.png' width=15px height=15px>";
+	}	else 	{
+		check.innerHTML = "<img src='Images/x.png' width=15px height=15px>";
+	}
+}
+
+function samePassword ()	{
+	for (var i = 0; i < document.getElementsByName("password")[1].value.length; i++) {
+		if (document.getElementsByName("password")[1].value[i] !== document.getElementsByName("password")[2].value[i])
+			return false;
+		else
+			return true;
+	}
 }
